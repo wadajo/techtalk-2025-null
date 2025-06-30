09985efc5af6946fec5416da1784618ab64ece35
julio.estravis <julio.estravis@globant.com>
2025-06-30 16:36:31 +0200

    WireMock para tests. REFACTOR

--- src/main/java/com/wadajo/nulltechtalk/GeneralController.java
+++ src/main/java/com/wadajo/nulltechtalk/GeneralController.java
@@ -1,20 +1,54 @@
 package com.wadajo.nulltechtalk;
 
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestClient;
 
-import java.time.Year;
+import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class GeneralController {
 
-    @GetMapping("/obras")
-    ResponseEntity<Obra> getRandomArtwork() {
+    @Value("${artworks.base-url}")
+    private String endpointUrl;
 
-        return ResponseEntity.ok(
-                new Obra("Nasca","Fragment", Year.of(600),null));
+    private RestClient restClient;
+
+    @PostConstruct
+    public void init() {
+        restClient = RestClient.builder()
+                .baseUrl(endpointUrl)
+                .build();
+    }
+
+    @GetMapping("/obras")
+    ResponseEntity<Obra> getRandomArtwork() throws IOException {
+        String rawResponse = restClient.get()
+                .retrieve()
+                .body(String.class);
+
+        var mapper = getObjectMapper();
+        var dataRawField = mapper.readTree(rawResponse).get("data");
+        var obraRandomEnLista = mapper.readValue(dataRawField.traverse(), new TypeReference<List<Obra>>() {});
+
+        return ResponseEntity.ok(obraRandomEnLista.getFirst());
+    }
+
+    private static ObjectMapper getObjectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.registerModule(new JavaTimeModule());
+
+        return mapper;
     }
 
 }
--- src/test/java/com/wadajo/nulltechtalk/acceptance/AcceptanceCriteriaIT.java
+++ src/test/java/com/wadajo/nulltechtalk/acceptance/AcceptanceCriteriaIT.java
@@ -1,23 +1,44 @@
 package com.wadajo.nulltechtalk.acceptance;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.test.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.wiremock.spring.ConfigureWireMock;
+import org.wiremock.spring.EnableWireMock;
 
+import java.io.File;
+import java.io.IOException;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static io.restassured.RestAssured.when;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableWireMock(
+        @ConfigureWireMock(port = 8888, baseUrlProperties = "${artworks.base-url}")
+)
 public class AcceptanceCriteriaIT {
 
     @LocalServerPort
     protected int port;
 
+    public static final String OBRA_RAW_STUBBING_FILE = "src/test/resources/stubbing/obra-raw.json";
+
     @Test
-    void debeDevolverUnaObraConFormatoCorrecto() {
+    void debeDevolverUnaObraConFormatoCorrecto() throws IOException {
+        JsonNode obraRaw = new ObjectMapper().readTree(new File(OBRA_RAW_STUBBING_FILE));
+
+        stubFor(get("/")
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withJsonBody(obraRaw)
+                ));
+
         when()
                 .get(String.format("http://localhost:%s/obras",port))
                 .prettyPeek()
@@ -25,7 +46,6 @@ public class AcceptanceCriteriaIT {
                 .assertThat()
                 .body("artist_title", Matchers.equalTo("Nasca"))
                 .statusCode(200);
-
     }
 
 }
